0326b4a474077b3805d7800230fcacfca3af103f
uloKK <supercooliobro@hotmail.com>
2017-07-13 16:58:25 +0300

    form validation by javascript
    the button is unclickable if not all of the fields are filled in. this
    also changes its style.

--- install/install.js
+++ install/install.js
@@ -4,7 +4,6 @@ var pass;
 var db;
 var button;
 function checkInput(){
-	setTimeout
 	host = document.forms["sql-data"]["host"].value;
 	user = document.forms["sql-data"]["user"].value;
 	pass = document.forms["sql-data"]["pass"].value;
@@ -12,10 +11,14 @@ function checkInput(){
 	db = document.forms["sql-data"]["db"].value;
 	if ((host != "") && (user != "") && (pass != "") && (db != "")){
 		button.removeAttribute("disabled");
+		document.getElementById("submitButton").classList.remove('disabled');
 		console.log("enable button")
 	} else {
 		button.setAttributeNode(document.createAttribute("disabled"));
 		console.log("disable button");
+		document.getElementById("submitButton").classList.add('disabled');
 	}
 }
+
+
 
